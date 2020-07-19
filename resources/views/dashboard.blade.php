05bc390f83d7e0af5828e89b87eec46d900fc6c0
Gaurav <gauravsoni7827@gmail.com>
2020-07-19 13:37:44 +0530

    Hg

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -723,12 +723,14 @@
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
               <li class="breadcrumb-item active">Dashboard v1</li>
-              <div class="image">
-  <div class="col-sm-6">
+              
+ <div class="col-sm-6">
             <h1 class="m-0 text-dark">Juhi Yadav</h1>
-          </div><!-
+          </div>
+              <div class="image">
+ 
                 
-          <img src="dist/img/urser2-160x160.jpg" width="160px" class="img-circle elevation-2" alt="User Image">
+         <img src="dist/img/urser2-160x160.jpg" width="160px" class="img-circle elevation-2" alt="User Image">
         </div>
             </ol>
           </div><!-- /.col -->
