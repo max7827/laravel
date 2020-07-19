169836eea98ebd34f1de068f38aa07045d147719
Gaurav <gauravsoni7827@gmail.com>
2020-07-19 13:41:06 +0530

    Yy

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -724,9 +724,9 @@
               <li class="breadcrumb-item"><a href="#">Home</a></li>
               <li class="breadcrumb-item active">Dashboard v1</li>
               
- <div class="col-sm-6">
+ 
             <h1 class="m-0 text-dark">Juhi Yadav</h1>
-          </div>
+          
               <div class="image">
  
                 
